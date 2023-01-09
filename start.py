48ee7c40fef459a5daddaa5758910df28d984e64
jcv12 <john.c.vayianos@gmail.com>
2023-01-08 20:40:27 -0500

    looks great need to add more touches'

--- start.py
+++ start.py
@@ -3,28 +3,31 @@ import os
 user_input = ''
 
 user_input = input("Which directory and files should we set up for you: (1)Web Basic (2)Web Full Stack (3)Python Script ")
+parent_dir = '/Users/john/Desktop/'
+
 
 print (user_input)
 
 if user_input == '1':
     print('Web Basic')
     directory = input("Directory Name: ")
-    parent_dir = '/Users/john/Desktop/'
     path = os.path.join(parent_dir, directory)
     os.mkdir(path)
     print("Directory '% s' created" % directory)
 
-    f = open("index.html", "a")
-    f.write("")
-    f.close()
+    textFilePath = '' + '/Users/john/Desktop/' + directory + '/' + 'index.html'
+    with open(textFilePath, 'w') as f:
+        f.write('')
 
-    f = open("style.css.py", "a")
-    f.write("")
-    f.close()
+    textFilePath = '' + '/Users/john/Desktop/' + directory + '/' + 'style.css'
+    with open(textFilePath, 'w') as f:
+        f.write('')
 
-    f = open("script.js", "a")
-    f.write("")
-    f.close()
+    textFilePath = '' + '/Users/john/Desktop/' + directory + '/' + 'script.js'
+    with open(textFilePath, 'w') as f:
+        f.write('')
+        print ('Created Files Succesfully!')
+    
 
 if user_input == '2':
     print('Script Python')
